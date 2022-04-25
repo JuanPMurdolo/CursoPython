591a3c8de172b7245fb812b05f4d4584ed35f105
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-04-25 16:47:51 -0300

    Strings

---
+++ Proyecto 1/Unidad5.py
@@ -0,0 +1,20 @@
+#Strings
+#Los strings pueden ser con comillas simples o dobles es indistinto
+
+# \ esta barra mas el string va a hacer que el string tenga un comportamiento particular
+#pasa lo mismo si usas ' en el ejemplo de abajo
+cadenaLiteral = "Esto es un ejemplo\"ejemplo\" de cadena"
+print(cadenaLiteral)
+
+#salto de linea \n
+cadenaSalto = "Esto es un ejemplo: \n Ejemplo de cadena \n con salto"
+print(cadenaSalto)
+
+#tabulacion \t
+cadenaTab = "Esto es un ejemplo: \t Ejemplo de cadena \t con tabulacion"
+print(cadenaTab)
+
+#\b saca un espacio entre
+#\f coloca el simbolo &
+#\r elimina todo lo que tenga delante
+
