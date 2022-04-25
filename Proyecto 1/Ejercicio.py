477edd208db37199402b48dcb6c61f8f3502816a
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-04-25 16:34:13 -0300

    Unidades 1 al 4

---
+++ Proyecto 1/Ejercicio.py
@@ -0,0 +1,24 @@
+#Ejercicio 1
+#Hacer la operacion matematica  (3+2) dividido (2*5). Todo elevado al cuadrado
+cuenta = ((3+2)/(2*5))**2
+print (cuenta)
+
+#Variante Ejercicio1
+cuenta = pow(((3+2) / (5*2)), 2)
+print(cuenta)
+
+#Ejercicio2
+#Una jugueteria tiene mucho exito en dos de sus productos: payasos y muñecas. Suele hacer venta por correo y la empresa 
+#de logistica les cobra  por peso de cada paquete, asi que deben calcular el peso de los payasos y de las muñecas que
+#saldran en cada paquete a demanda. Cada payaso pesa 112g y cada muñeca 75g. Un cliente frecuente pide la cantidad
+#de 23 payasos y 54 muñecas, realiza un programa que muestre el peso total de toda la venta
+
+PAYASO = 112
+MUNIECA = 75
+
+totalVenta = (23 * PAYASO) + (54 * MUNIECA)
+totalVenta = totalVenta / 100
+print("el peso total de la venta es " + str(totalVenta) + " kilos")
+
+#Ejercicio3
+#
---
+++ Proyecto 1/Introduccion.py
@@ -0,0 +1,48 @@
+#--------------------------------Variables en Python
+#<nombre var> = <valor>
+#HOla
+nombre = "juan carlos"
+numero = 10
+flotante = 3.14
+
+print(type(nombre))
+print(nombre)
+print (type(numero))
+print(numero)
+print(type(flotante))
+print(flotante)
+
+#3 Comillas simples para comentarios de varias lineas
+'''En Pythono existen las constantes formalmente pero como una forma comun de  usarlas es
+Definir en mayusuclas el nombre de la variable sabiendo que va a ser una constante'''
+CONSTANTE = 100
+
+#--------------Seccion 4----------------------
+#Numeros
+#En lo que es numeros para python existe entero y flotante, nada mas
+
+numeroUno = 140
+numeroDos = 100.10
+
+#float(<numero entero>) convierte a flotante un  numero  entero o un string si es posible, hay que asignarlo para usarlo
+print(type(numeroUno))
+print(numeroUno)
+numeroUno = float(numeroUno)
+print(type(numeroUno))
+print(numeroUno)
+
+#int(<numero flotante>) convierte un flotante o un string a un numero entero, hay que asignarlo
+print(type(numeroDos))
+print(numeroDos)
+numeroDos = int(numeroDos)
+print(type(numeroDos))
+print(numeroDos)
+
+#------------------Operaciones Aritmeticas-----------------------
+#Suma Suma = numero + numero
+#Resta Resta = numero - numero
+#Division Division = numero / numero
+#Multiplicacion Multiplicacion = numero * numero
+#Exponenciacion Exponeenciacion = numero ** numero
+#Division Entera DivisionEntera = numero // numero
+#Tomar el resto de una division Resto = numero % numero
