268c2fb4d28c431106282acec414b5660b9070d6
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-04-26 16:57:57 -0300

    Ejercicios5

--- Proyecto 1/Ejercicios/Ejercicios5.py
+++ Proyecto 1/Ejercicios/Ejercicios5.py
@@ -1,1 +1,25 @@
 #Unidad8
+#Crear un programa que pida al usuario una  letra, y si es vocal, muestre el mensaje es vocal. Sino decirle al usuario que no es vocal 
+letra = input("Ingrese una vocal: ")
+
+if letra.lower() == "a":
+    print("Esta vocal es la A")
+elif letra.lower() == "e":
+    print("Esta vocal es la E")
+elif letra.lower() == "u":
+    print("Esta vocal es la U")
+elif letra.lower() == "i":
+    print("Esta vocal es la I")
+elif letra.lower() == "o":
+    print("Esta vocal es la O")
+else:
+    print("no es una vocal")
+
+#Ejercicio2
+#Escribir un programa que, dado un numero entero, muestre su valor absoluto.
+
+ingreso = int(input("ingrese un numero: "))
+if (ingreso < 0):
+    print("valor absoluto de ingreso es:", ingreso * -1)
+else:
+    print("valor absoluto de ingreso es:", ingreso)
