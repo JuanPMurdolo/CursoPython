3e1c6416ba206663d24a5393d5895a68f010c351
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-04-26 17:26:34 -0300

    Create Unidad9.py

---
+++ Contenido del curso/Teoria/Unidad9.py
@@ -0,0 +1,21 @@
+#Listas, Tuplas y Diccionarios
+
+#Listas
+#La lista es un tipo de coleccion ordenada. Seria equivalente a lo que en otros lenguajes se conoce por arrays, o vectores.
+
+#Se usan corchetes[] y ,
+lista = ["juan", "Pepe", "Rogelio"]
+
+
+#Tuplas
+#Una tupla es una secuencia de valores similar a una lista. La principal diferencia es que las tuplas son inmutables
+#Se forma con parentesis () y ,
+tupla = ("Jugar", 23.78, "Juan", "Tupla")
+
+#Diccionarios
+#Los diccionarios deben su nombre  a que son colecciones que relacionan una clave y un valor.
+#Se forma con llaves {}, : en la clave y , para separar
+diccionario = {"Uno": 1, "Dos": 2, "Tres": 3}
+#Uno Dos y Tres serian Claves, 1 2 y 3 serian valores
+print(diccionario["Uno"])
+#Asi se imprime un diccionario
